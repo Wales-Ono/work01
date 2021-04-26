a68f9c3d5c152702f03d85c311d627bed14e427e
Ono <ono@wales.jp>
2021-04-26 13:32:18 +0900

    0426-13:32

--- 課題2/2-1(文法編)/Kadai3.java
+++ 課題2/2-1(文法編)/Kadai3.java
@@ -3,10 +3,15 @@ import java.util.List;
 
 public class Kadai3{
     public static void main(String[] args){
-        List<String> list = new ArrayList<String>();
-        for(String arg : args){
-            list.add(arg);
-            System.out.println(arg);
+        // List<String> list = new ArrayList<String>();
+        // for(String arg : args){
+        //     list.add(arg);
+        //     System.out.println(arg);
+        // }
+        @SafeVarargs
+        List<String> lists = Arrays.asList(args);
+        for(String list : lists){
+            System.out.println(list);
         }
     }
 }
